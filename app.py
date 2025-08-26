64d17cfedc457a2840d5581b7731b7d554476c80
Dinesh Bandaru <bandarudinesh20@gmail.com>
2025-08-26 05:48:16 -0700

    update reader's

--- app.py
+++ app.py
@@ -17,4 +17,63 @@ parser.add_argument("-i","--index",help="reader index, start from zero.")
 args = parser.parse_args()
 
 #Default Values
+text_to_read ="Hi there! I'll read text for you."
+gender ="male"
+language="english"
+accent="us"
+
+#if text args passed in cli
+if args.text:
+    text_to_read=args.text
+
+#if language args passed in cli
+if args.language:
+    language=args.language
+    if language=="hindi":
+        accent="Indian"
+
+#if accent args passed in cli
+if args.accent:
+    accent=args.accent
+
+#if gender args passed in cli
+if args.gender:
+    gender=args.gender
+
+#filters the languages and accents
+def filter_rule(voice,gender,language,accent,default):
+    if default:
+        return voice.gender in gender and voice.language[0]==(language + '_' +accent)
+    return  voice.language[0]==(language + '_' +accent) or voice.name in ["default","Alex"]
+
+#filtering voices based on given criteria
+def filter_voice(voices,gender,language,accent,default=True):
+    filter_list =[voice for voice in voices if filter_rule(voice,gender,language,accent,default)]
+    if len(filter_list):
+        return filter_list
+    return filter_voice(voices,gender,language,accent,False)
+
+#update reader's language,accent and gender
+def update_language(reader,language,accent,gender):
+    #Audio Type Selection Criteria
+    languages={"english":"en","hindi":"hi"}
+    accents ={"indian":"IN","us":"US","australia":"AU","uk":"GB"}
+    genders={"male":["voiceGenderMale","male"],"female":["voiceGenderFemale","female"],"none":["None"]}
+
+    #getting list of filtered voices based on selection criteria
+    filtered_voice_list=filter_voice(reader.getproperty('voices'),genders[gender],languages[language],accents[accent])
+
+    if len(filtered_voice_list):
+        #Displaying the details of filtered voices
+        print("AVAILABLE READERS")
+
+
+
+
+
+
+
+
+
+
 
